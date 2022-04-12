a7d271d379b33594b0f803d5d0d5fa683e3bcc47
Ravindar Reddy Chintala <ravindra1507@gmail.com>
2022-04-12 10:43:45 +0530

    Connection string

--- App_Code/VehicleDBMngr.cs
+++ App_Code/VehicleDBMngr.cs
@@ -15,7 +15,7 @@ public class VehicleDBMgr
       object obj_lock = new object();
       public VehicleDBMgr()
       {
-          conn.ConnectionString = @"SERVER=120.138.9.118;DATABASE=vyshnavi_sales_2016;UID=vyshnavi_root;PASSWORD=Vyshnavi@123;";
+          conn.ConnectionString = @"SERVER=182.18.162.51;DATABASE=vyshnavi_sales_2022;UID=admin;PASSWORD=CK!uM$Btz2#4c$;";
       }
     public  bool insert(MySqlCommand _cmd)
     {
